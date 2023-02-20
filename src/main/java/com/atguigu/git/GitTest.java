1f6de5c85d137ca539438957792a5e587fb42756
zyf_s <zyf_s@qq.com>
2023-02-20 17:16:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @date 2023/2/20 - 17:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
